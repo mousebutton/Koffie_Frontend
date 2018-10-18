451c4242b0c5cefc7be25d4e36b5a8355cca639b
TimBlanken <tim.blanken@alvant.nl>
2018-10-18 18:32:15 +0200

    test

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,6 @@ import Home from '@/components/Home'
 import Login from '@/components/Login'
 import Register from '@/components/Register'
 import UserPage from '@/components/UserPage'
-<<<<<<< HEAD
 import Admin from '@/components/Admin'
 import Unauthorized from '@/components/Unauthorized'
 
@@ -95,48 +94,6 @@ router.beforeEach((to, from, next) => {
     // The requested url not requiresAuth
     else {
         next()
-=======
-import Canvas from '@/components/Canvas'
-
-Vue.use(Router)
-
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'About',
-      component: About
-    },
-    {
-      path: '/contact',
-      name: 'Contact',
-      component: Contact
-    },
-    {
-      path: '/login',
-      name: 'Login',
-      component: Login
-    },
-    {
-      path: '/register',
-      name: 'Register',
-      component: Register
-    },
-    {
-      path: '/profile',
-      name: 'UserPage',
-      component: UserPage
-    },
-    {
-      path: '/canvas',
-      name: 'Canvas',
-      component: Canvas
->>>>>>> ac2bb9b6daf3fbc746be913e1ae5e01a6d6c3b1f
     }
 })
 
