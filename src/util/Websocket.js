6c8bbbc7b04d4b0eefd6901701735c6a2fec60a5
TimBlanken <tim.blanken@alvant.nl>
2018-12-18 11:46:38 +0100

    implement get coffee round

--- src/util/Websocket.js
+++ src/util/Websocket.js
@@ -22,22 +22,25 @@ export default new class WebsocketUtil {
 
   // Methods to register the websocket connection
   connectWebsocket() {
+    let user = JSON.parse(localStorage.getItem('user'));
     this.socket = new SockJS("http://localhost:8080/websocket-endpoint");
     this.stompClient = Stomp.over(this.socket);
+    console.log(user.department);
     this.stompClient.connect(
       {},
       connectEvent => {
         this.connected = true;
         // Subscribe to the topics to receive messages from the server
-        this.stompClient.subscribe("/global-message/chat", msg => {
-          messages.push(msg.body);
+        this.stompClient.subscribe("/global-message/coffeeRound/" + user.department, msg => {
+          alert(msg.body + ' gaat koffie halen');
+          orders.splice(0);
+
         }),
           this.stompClient.subscribe("/global-message/user", msg => {
             notifications.push(msg.body);
           });
-          this.stompClient.subscribe("/global-message/newOrder", msg => {
-            orders.push(JSON.parse(msg.body));
-            console.log(orders);
+          this.stompClient.subscribe("/global-message/newOrder/" + user.department, msg => {
+              orders.push(JSON.parse(msg.body));
           });
       },
       error => {
