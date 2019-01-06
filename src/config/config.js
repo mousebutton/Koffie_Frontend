48311d7eaee46072cd79b3e93582842604450528
DESKTOP-HUUB\Huub <huubverdel@gmail.com>
2019-01-06 20:13:22 +0100

    AWS URL

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
 export default {
-  //url: 'http://ec2-18-191-130-224.us-east-2.compute.amazonaws.com:8080'
-  url: 'http://localhost:8080'
+  url: 'http://ec2-18-191-130-224.us-east-2.compute.amazonaws.com:8080'
+  //url: 'http://localhost:8080'
 };
